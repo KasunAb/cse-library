f51a071b562286e95a47e1f3ea3fa0d3dbc4dc5a
Krishan <krishan.19@cse.mrt.ac.lk>
2021-09-23 11:22:30 +0530

    make the factorial function

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,11 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    public static int factorial(int n){
+        if (n >= 1)
+            return n*factorial(n-1);
+        else
+            return 1;
+    }
+
 }
