8e05824407facb177debca623e20f15fa094c543
abinesht <abinesht12@gmail.com>
2021-09-23 11:28:22 +0530

    fibonacci method

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 
 public class MathOperation {
+    int f1 = 0;
+    int f2 = 1;
+    int fn;
 
     public static double add(double... operands)
     {
@@ -13,6 +16,20 @@ public class MathOperation {
     public static double subtract(double operand1, double operand2)
     {
         return operand1 - operand2;
+    }
+
+    public static int fib(n){
+        if (n==1){
+            fn=1;
+        }
+        if(n>=2){
+            for (int i=1; i<n; i++){
+                fn = f1+f0;
+                f0 = f1;
+                f1=fn
+            }
+        }
+        return fn;
     }
 
 }
